2833593d0718bf42f3f3e4253e8e634da17f3edb
dinhtungtp <tung6192@gmail.com>
2017-06-08 13:31:56 +0700

    test authenticated users

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule Rumbl.PageControllerTest do
   use Rumbl.ConnCase
 
+  @tag pageindex: "yep"
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Rumb.io"
   end
 end
